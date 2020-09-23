ab4ea3be3aecf2239992132c018a7e836a5c91be
akam alam <arundelfootball23@gmail.com>
2020-09-23 04:49:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  linking= 
+  puts "it's Dangerous To Go Alone! Take This."
+end 
+
+def all_phrases 
+  
+end
